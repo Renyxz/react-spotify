9300b27f0fd2c7b715d12a8ca43e64efc922e2d9
Ren <ren.yxz.dev@gmail.com>
2018-04-26 18:25:35 -0400

    Migrated handleQuery and auth button to new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
-import { search } from '../inc/spotify-api';
-import AUTH_URI from '../inc/auth_uri';
-import hashParams from '../inc/hashParams';
+import { Redirect } from 'react-router-dom';
+import { hashParams } from '../inc/spotify-api';
 
 
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-      accessToken: '',
-
-    }
-  }
-
-
 
   componentDidMount() {
     
@@ -37,59 +24,32 @@ class App extends Component {
   
 
 
-  redirectToAuth() {
-    window.location = AUTH_URI;
-    
-  }
+  render() {
 
+    const accessToken = window.sessionStorage.token;
 
-  handleQuery() {
-    
-    const accessToken = this.state.accessToken;
+    if(accessToken) {
 
-    if(!accessToken) { return; }
-    
-    // Search query
-    const query = {
+      return(
 
-      accessToken, // Required
-      keyword: encodeURIComponent('chainsmoker'), // Required
-      searchType: ['album', 'artist', 'playlist', 'track'] // Required
+        <Redirect to="/browse" />
+
+      );
 
     }
 
-    // Search - Make request to Spotify API:
-    const promise = search(query);
-    
-    // If request is successful:
-    promise.then( (data) => {
-      console.log(data);
-    });
 
-    // If request failed:
-    promise.catch( (error) => {
-      console.log(error);
-    });
 
-    console.log(accessToken, window.sessionStorage);
-
-  }
-
-  render() {
-    // console.log(this.state.accessToken);
     return (
 
       <div className="">
 
-        { this.handleQuery() }
 
-        <button onClick={ () => this.redirectToAuth() } >
-          GET STARTED
-        </button>
       
       </div>
     
-  );
+    );
+
   }
 }
 
