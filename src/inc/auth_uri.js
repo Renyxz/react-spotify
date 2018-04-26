1fd02708e383e28f0bf9e5924745a77741beb26f
Ren <ren.yxz.dev@gmail.com>
2018-04-26 10:00:07 -0400

    Restructured code and migrated code for getting auth URI to spotify-api.js

--- src/inc/auth_uri.js
+++ src/inc/auth_uri.js
@@ -1,12 +1,15 @@
-// Implicit Grant Auth variables
-const client_id = 'df2860e01b3444c497c9d5580e5bc6c6';
-const response_type = 'token';
-const redirect_uri = 'http://localhost:3000';
+import { getAuthURI } from './spotify-api'
 
-// Auth URI
-let AUTH_URI = 'https://accounts.spotify.com/authorize';
-AUTH_URI += `?client_id=${ encodeURIComponent(client_id) }`;
-AUTH_URI += `&redirect_uri=${ encodeURIComponent(redirect_uri) }`
-AUTH_URI += `&response_type=${ response_type }`
+// Implicit Grant Auth variables
+const client = {
+
+    client_id: 'df2860e01b3444c497c9d5580e5bc6c6',
+    response_type: 'token',
+    redirect_uri: 'http://localhost:3000',
+
+};
+
+// Gets the authentication URI
+const AUTH_URI = getAuthURI(client);
 
 export default AUTH_URI;
