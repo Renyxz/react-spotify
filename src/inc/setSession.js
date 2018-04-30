806321f56c65983c377492f9708f8ab91295e57d
Ren <ren.yxz.dev@gmail.com>
2018-04-30 12:35:42 -0400

    Code for setting session with auth returned hash params. Migrated from App component

---
+++ src/inc/setSession.js
@@ -0,0 +1,30 @@
+import { hashParams } from './spotify-api';
+
+// Set session
+const hash = window.location.hash;
+const params = hashParams(hash);
+const accessToken = params.access_token;
+const expiresIn = params.expires_in;
+
+// If access token has not been saved in session storage
+const date = new Date();
+
+export const setSession = () => {
+
+    if(accessToken) {
+    
+        window.sessionStorage.token = accessToken;
+    
+        if(!window.sessionStorage.expiryTime) {
+        
+        const tokenLife = expiresIn * 60000;
+        const timestamp = date.getTime();
+        const expiryTime = timestamp + tokenLife;
+        window.sessionStorage.expiryTime = expiryTime;
+        
+        }
+        
+    }
+
+}
+
