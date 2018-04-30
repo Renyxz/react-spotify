7518ccb9ad422759b1c6d957e192e0a21d432ec7
Ren <ren.yxz.dev@gmail.com>
2018-04-30 12:30:20 -0400

    Migrated code for setting session to setSession file

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,16 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { hashParams } from '../inc/spotify-api';
 
 
 
 class App extends Component {
 
-  componentDidMount() {
-    
-    const hash = window.location.hash;
-    const accessToken = hashParams(hash).access_token;
-    
-    if(accessToken) {
-
-      this.setState({ accessToken });
-
-      window.sessionStorage.token = accessToken;
-
-    }
-    // console.log(accessToken);
-
-  }
-  
-
-
   render() {
-
-    const accessToken = window.sessionStorage.token;
-
-    if(accessToken) {
-
-      return(
-
-        <Redirect to="/browse" />
-
-      );
-
-    }
-
-
 
     return (
 
       <div className="">
 
-
+      
       
       </div>
     
