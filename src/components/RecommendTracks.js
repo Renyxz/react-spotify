fd0a1298d0923198d08c8d4fa78f46eef5819358
Ren <ren.yxz.dev@gmail.com>
2018-05-09 10:21:08 -0400

    Component for recommend tracks function with React-Redux

---
+++ src/components/RecommendTracks.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { getRecommendations } from '../inc/spotify-api';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchRecommendations } from '../actions';
+
+
+const RecommendTracks = ({ seed, fetchRecommendations }) => {
+
+    const handleRecommend = (seed) => {
+        
+        const query = {
+            accessToken: window.sessionStorage.token
+        };
+
+        const promise = getRecommendations(query, seed);
+
+        promise.then( res => {
+            // TODO: Find out why error is being sent to action creator instead of being catched here.
+            fetchRecommendations(res);
+            console.log('Recommendation: ', res);
+        });
+
+        promise.catch( error => {
+            console.log('Error: ', error);
+        });
+
+    }
+
+    return(
+
+        <i className="fas fa-angle-double-right mr-3" onClick={ () => handleRecommend(seed) } >
+        </i>
+
+    );
+
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({ fetchRecommendations }, dispatch);
+};
+
+export default connect(null, mapDispatchToProps)(RecommendTracks);
