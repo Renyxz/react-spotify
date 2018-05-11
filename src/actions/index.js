1ba87a922c11435b836b0ae339274699951945ed
Ren <ren.yxz.dev@gmail.com>
2018-05-11 17:04:40 -0400

    Added action creator for getting tracks of a category playlist

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ export const actions = {
 
     FETCH_SEARCH_RESULT: 'FETCH_SEARCH_RESULT',
     FETCH_RECOMMENDATIONS: 'FETCH_RECOMMENDATIONS',
+    FETCH_CATEGORY: 'FETCH_CATEGORY',
 
 };
 
@@ -23,6 +24,15 @@ export const fetchRecommendations = (data) => {
     console.log('fetchRecommendations: ', data);
     return {
         type: actions.FETCH_RECOMMENDATIONS,
+        payload: data
+    };
+
+}
+
+export const fetchCategory = (data) => {
+    console.log('fetchCategory: ', data);
+    return {
+        type: actions.FETCH_CATEGORY,
         payload: data
     };
 
