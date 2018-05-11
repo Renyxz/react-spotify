ec288f21b023ae536274137e3864b12ad8f4adef
Ren <ren.yxz.dev@gmail.com>
2018-05-11 17:08:25 -0400

    Modified categories list component into a modal

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { checkSession } from '../inc/checkSession';
-import { getCategoryPlaylists } from '../inc/spotify-api';
+import { getCategoryPlaylists, getPlaylistTracks } from '../inc/spotify-api';
 
 
 class Categories extends Component {
@@ -45,6 +45,29 @@ class Categories extends Component {
     }
     
 
+    // get playlist of a category
+    handleCategoryPlaylist(params) {
+
+        const query = {
+            accessToken: window.sessionStorage.token,
+            user_id: params.user_id,
+            playlist_id: params.playlist_id
+        };
+
+        const promise = getPlaylistTracks(query);
+
+        promise.then( res => {
+            console.log(res);
+            // TODO: dispatch getPlaylistTracks
+        });
+
+        promise.catch( error => {
+            console.log(error);
+        });
+
+    }
+
+
     render() {
 
         const categories = this.state.data; console.log(categories);
@@ -62,33 +85,53 @@ class Categories extends Component {
 
 
         return(
-    
-            <div className="">
-    
-                <h4>Genres</h4>
-    
-                <div className="row">
-                    
-                    {
-                        categories.map( (data, i) => {
-                            const item = data.playlists.items[0];
-                            const imgURL = item.images[0].url;
+            
+            <div className="modal fade" id="categoriesModalCenter" tabIndex="-1" role="dialog" aria-labelledby="categoriesModalCenterTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
 
-                            return(
+                    <div className="modal-content">
 
-                                <div key={ i } className="col-lg-2 col-md-3 col-6 mb-4">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="categoriesModalCenterTitle">Genres</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
 
-                                    <img className="img-fluid" src={ imgURL } alt={ item.name }/>
+                        <div className="modal-body">
+                            
+                            <div className="row">
+                                
+                                {
+                                    categories.map( (data, i) => {
+                                        const item = data.playlists.items[0];
+                                        const imgURL = item.images[0].url;
 
-                                </div>
+                                        const params = {
+                                            user_id: item.owner.id,
+                                            playlist_id: item.id
+                                        };
 
-                            );
+                                        return(
 
-                        })
-                    }
+                                            <div key={ i } className="col-sm-6 mb-4" onClick={ () => this.handleCategoryPlaylist(params) } data-dismiss="modal">
+
+                                                <img className="img-fluid" src={ imgURL } alt={ item.name } width="100%"/>
+
+                                            </div>
+
+                                        );
+
+                                    })
+                                }
+
+                            </div>
+
+                        </div>
+
+                    </div>
 
                 </div>
-    
             </div>
     
         );
